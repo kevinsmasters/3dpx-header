4d134238a899be3d958bce2423652c6d30a339b9
kevinsmasters <gmail@kevinmasters.com>
2022-06-03 21:05:14 -0400

    fixes slider bug

--- js/main.js
+++ js/main.js
@@ -1,37 +1,43 @@
 //console.log("meowdy - js loaded");
 (function ($) {
   $(document).ready(function () {
-    console.log("jquery init");
+    //console.log("jquery init");
     const forward = () => {
       //console.log('forward');
       let currDot = $("#timeLine .active");
       let curIndex = currDot.prevAll().length;
       let liLen = $("#timeLine ul li").length;
-      console.log("Index: " + curIndex);
-      console.log("Length: " + liLen);
+      //console.log("Index: " + curIndex);
+      //console.log("Length: " + liLen);
       if (curIndex < liLen - 2) {
         currDot.next("li").addClass("active");
         currDot.removeClass("active");
         $(".slide[data-slide=" + curIndex + "]").removeClass("shown");
         $(".slide[data-slide=" + (curIndex + 1) + "]").addClass("shown");
-      } else {
+      } else if (curIndex != 8) {
+        //console.log('else: ', curIndex);
         currDot.removeClass("active");
+        $("#timeLine li:nth-child(9)").addClass("active");
         setTimeout(function () {
-          $("#timeLine li:nth-child(1)").addClass("active");
+          $("#timeLine li:nth-child(9)").removeClass("active");
+          //$("#timeLine li:nth-child(1)").addClass("active");
           $(".slide").removeClass("shown");
           $(".slide[data-slide=0]").addClass("shown");
         }, 2500);
+      } else {
+        //console.log('idk mybffjill');
+        $("#timeLine li:nth-child(1)").addClass("active");
       }
     };
     //forward();
     $(".tl-item:not(.flag)").on("click", function () {
-      console.log("clicked");
+      //console.log("clicked");
       //forward();
       $("#timeLine li").removeClass("active");
       $(this).addClass("active");
       //$(this).parent().css('border', '1px solid red');
       var parentIndex = $(this).prevAll().length;
-      console.log("parent: ", parentIndex);
+      //console.log("parent: ", parentIndex);
       $(".slide").removeClass("shown");
       $(".slide[data-slide=" + parentIndex + "]").addClass("shown");
     });
