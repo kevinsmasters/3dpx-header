43fcf7fc5b9371305ef07f5fd4ed5a85a3777e78
Kevin Masters <irimiuke@gmail.com>
2024-10-12 22:04:33 -0400

    add line breaks

--- includes/threedpx-header-scripts.php
+++ includes/threedpx-header-scripts.php
@@ -1,10 +1,12 @@
 <?php
 // add scripts
 function tdpx_add_scripts(){
+
     // add main css
     wp_enqueue_style('tdpx-main-style', plugins_url(). '/3dpx-header/css/style.css');
     // add main JS
     wp_enqueue_script('tdpx-main-script', plugins_url(). '/3dpx-header/js/main.js', array('jquery'));
+
 }
 
 add_action('wp_enqueue_scripts', 'tdpx_add_scripts');
