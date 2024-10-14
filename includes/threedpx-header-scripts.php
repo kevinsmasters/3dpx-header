6fc088c2093a67aae34564ae13c74776eb867675
Kevin Masters <irimiuke@gmail.com>
2024-10-14 14:09:37 -0400

    clean up many line length overflows

--- includes/threedpx-header-scripts.php
+++ includes/threedpx-header-scripts.php
@@ -3,9 +3,15 @@
 function tdpx_add_scripts(){
 
     // add main css
-    wp_enqueue_style('tdpx-main-style', plugins_url(). '/3dpx-header/css/style.css');
+    wp_enqueue_style(
+        'tdpx-main-style', plugins_url()
+            . '/3dpx-header/css/style.css'
+    );
     // add main JS
-    wp_enqueue_script('tdpx-main-script', plugins_url(). '/3dpx-header/js/main.js', array('jquery'));
+    wp_enqueue_script(
+        'tdpx-main-script', plugins_url()
+            . '/3dpx-header/js/main.js', array('jquery')
+    );
 
 }
 
